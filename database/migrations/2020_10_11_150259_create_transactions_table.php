d92261d664ede771dd190770fc749cebaec9684a
wesley601 <wesleypassos601@gmail.com>
2020-10-12 15:16:06 -0300

    Add transaction migrations

---
+++ database/migrations/2020_10_11_150259_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->dateTimeTz('date');
+            $table->decimal('value', 999, 2);
+            $table->enum('type', ['input', 'output']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
